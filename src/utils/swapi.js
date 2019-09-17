cef1348a6492895ab1871a8bd95ee41ec1bdf2f4
660-ilegra <cesar_martinez22@hotmail.com>
2019-09-17 19:57:34 -0300

    swapi integration tests

--- src/utils/swapi.js
+++ src/utils/swapi.js
@@ -3,7 +3,10 @@
 const ROOT_URL = 'https://swapi.co/api';
 
 class Swapi {
-  async get(url) {
+  async get(url, useRoot = false) {
+    if (useRoot) {
+      url = `${ROOT_URL}/${url}`;
+    }
     const response = await fetch(url);
     const data = await response.json();
     return data;
@@ -11,7 +14,7 @@ class Swapi {
 
   // Get all movies
   films() {
-    return this.get(`${ROOT_URL}/films`);
+    return this.get('films', true);
   }
 
   // Get content from resources URL array
